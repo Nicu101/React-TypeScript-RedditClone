41d092a8dc44ec62aa80a8c980f853f19997b009
Nicu <vasilenicolae.101@gmail.com>
2021-06-02 18:21:40 +0300

    created a Post table

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import { MikroORM } from "@mikro-orm/core";
+import { _prod_ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig);
+
+    const post = orm.em.create(Post, {title: 'the first post'});
+    await orm.em.persistAndFlush(post);
+};
+
+main().catch(err => {
+    console.error(err);
+})
+
+
 console.log("Hello World!");
